099e284db58a850ff2aff3dcb4d436cbed6b454d
Eric Spencer <spencer22eric@gmail.com>
2022-04-21 18:49:39 -0400

    display table rows as array

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const res = require('express/lib/response');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,10 +7,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Maggiemybuddy1!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
 });
 
 app.use((req, res) => {
