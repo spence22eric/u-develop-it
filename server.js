c174a99efeb3ddd45dca867ab300d8d0b529b79b
Eric Spencer <spencer22eric@gmail.com>
2022-04-21 23:41:05 -0400

    create, read, delete entry

--- server.js
+++ server.js
@@ -11,15 +11,41 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'Maggiemybuddy1!',
+        password: '12345',
         database: 'election'
     },
     console.log('Connected to the election database.')
 );
 
-db.query('SELECT * FROM candidates', (err, rows) => {
-    console.log(rows);
-});
+// db.query('SELECT * FROM candidates', (err, rows) => {
+//     console.log(rows);
+// });
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?, ?, ?, ?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// db.query('DELETE FROM candidates WHERE id = ?', 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 app.use((req, res) => {
     res.status(404).end();
